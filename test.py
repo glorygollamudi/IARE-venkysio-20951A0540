151b5a93eaf3b9d414ad18e3a419c3cacdce2d98
glorygollamudi <113373896+glorygollamudi@users.noreply.github.com>
2023-02-25 11:18:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-x=int(input())
-y=int(input())
+x=int(input("Enter no. of Toys:"))
+y=int(input("Enter amount:"))
+z=int(input("enter the amount left:"))
+a=y//x
+b=y+z
+print("Cost of each toy:",a)
+print("Total amount :",b)
